3d0e71a970a1f1a0400233ef3d71bbc6f07bd2f3
Yassine-Fellous <91788542+Yassine-Fellous@users.noreply.github.com>
2023-10-04 23:23:11 +0200

    Add files via upload

---
+++ MazeRunner/src/main/java/mazerunner/Main.java
@@ -0,0 +1,20 @@
+package mazerunner;
+
+import mazerunner.MazeGenerator.SimpleImperfectMazeGenerator;
+import mazerunner.MazeGenerator.SimplePerfectMazeGenerator;
+
+public class Main {
+    private static void launchGenerator(ParamVerif pv) {
+        if (pv.getMethod() == 1) {
+            if(pv.getPerfect())
+                new SimplePerfectMazeGenerator(pv.getWidth(), pv.getHeight());
+            else
+                new SimpleImperfectMazeGenerator(pv.getWidth(), pv.getHeight());
+        }
+    }
+
+    public static void main(String[] args) {
+        ParamVerif pv = new ParamVerif(args);
+        launchGenerator(pv);
+    }
+}
